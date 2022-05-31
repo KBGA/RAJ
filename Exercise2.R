4f9c9fbfbc8a2840bd1f190a299f02189c8037b9
KBGA <kouayim.bonga@gmail.com>
2022-05-31 13:15:53 +0200

    Update Exercise2.R

--- Exercise2.R
+++ Exercise2.R
@@ -3,10 +3,21 @@
 
 
 
-
+#Zuerst muss das esoph dataset geladen werden
 data("esoph")
+
+#Jetzt muss ich wissen was für einen Datentyp das Dataset hat
+class(esoph)
+
+#Das esoph-Dataset ist ein data.frame. Ich kann dann die Größe/Dimension ermitteln.
+dim(esoph) #Das esoph-Dataset hat 88 Zeilen und 5 Spalten
+
+
 
 esoph
 
-summary(esoph)
 
+colnames(esoph)
+
+class(esoph)
+dim(esoph)
